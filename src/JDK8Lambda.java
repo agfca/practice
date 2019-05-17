b8c292623e6044f90f4fa89b42954a73588184dd
agfca <a.gjb@outlook.com>
2019-05-17 19:47:34 +0800

    jdk8Lambda

---
+++ src/JDK8Lambda.java
@@ -0,0 +1,40 @@
+/**
+ * @date 2019-05-17 13:02
+ *
+ * (parameters) -> expression
+ * 或
+ * (parameters) ->{ statements; }
+ *
+ * 重要特征:
+ *
+ * 可选类型声明：不需要声明参数类型，编译器可以统一识别参数值。
+ * 可选的参数圆括号：一个参数无需定义圆括号，但多个参数需要定义圆括号。
+ * 可选的大括号：如果主体包含了一个语句，就不需要使用大括号。
+ * 可选的返回关键字：如果主体只有一个表达式返回值则编译器会自动返回值，大括号需要指定明表达式返回了一个数值。
+ */
+public class JDK8Lambda {
+    public static void main(String[] args) {
+        //类型声明
+        MathOperation add = (int a,int b)->a+b;
+        //不用类型声明
+        MathOperation sub = (a,b)->a-b;
+        //大括号中的返回语句
+        MathOperation mul = (a, b) -> {return a*b;};
+        //没有大括号和返回语句
+        MathOperation div = (a,b) -> a/b;
+
+
+        int result = add.operation(1,2);
+
+        JDK8Lambda jdk8Lambda = new JDK8Lambda();
+        System.out.println("2 x 3 = "+jdk8Lambda.operation(2,3,add));
+        System.out.println("ss");
+
+    }
+    interface MathOperation{
+        int operation(int a,int b);
+    }
+    private int operation(int a,int b,MathOperation mathOperation){
+        return mathOperation.operation(a,b);
+    }
+}
---
+++ src/NewPointOfJDK8.java
@@ -0,0 +1,123 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class NewPointOfJDK8 {
+
+    /**
+     * JDK8 新特性
+     * @Date: 2019/5/17 10:22
+     */
+
+    /**
+     * Lambda 表达式 Lambda允许把函数作为一个方法的参数（函数作为参数传递进方法中）。
+     * @Date: 2019/5/17 10:30
+     *
+     *
+     * Collections.sort could be replaced with List.sort
+     * inspection 视察，审视
+     * comparator
+     * anonymous 匿名的
+     * expression
+     * syntax
+     * reference
+     * natural 自然的
+     * chain 链子
+     */
+    public static void main(String[] args) {
+        List<String> names1 = new ArrayList<String>();
+        names1.add("Google ");
+        names1.add("Runoob ");
+        names1.add("Taobao ");
+        names1.add("Baidu ");
+        names1.add("Sina ");
+
+        List<String> temp = Arrays.asList(Arrays.copyOf(names1.toArray(new String[names1.size()]),names1.size()));
+        List<String> names2 = new ArrayList<>(temp);
+        //UnsupportedOperationException  克隆异常  Arrays$ArrayList
+        names2.add("Gaoogle ");
+        names2.add("Sina ");
+        names2.add("Sana ");
+
+        sortUsingJava7(names1);
+
+        //sortUsingJava8_1(names2);
+
+        sortUsingJava8_4(names2);
+
+        System.out.printf("sssss/t/n\t22");
+    }
+
+    private static void sortUsingJava7(List<String> names){
+        Collections.sort(names, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                return s1.compareTo(s2);
+            }
+        });
+    }
+    private static void sortUsingJava8_1(List<String> names){
+        Collections.sort(names,(s1,s2)-> s1.compareTo(s2));
+    }
+    /*String.compareTo 方法：
+    compareTo() 的返回值是int, 它是先比较对应字符的大小(ASCII码顺序)*/
+    private void sortUsingJava8_2_1(List<String> names){
+        Collections.sort(names,Comparator.comparing(String::toString));
+    }
+    //判断 年龄
+    //return (x < y) ? -1 : ((x == y) ? 0 : 1);
+    private void sortUsingJava8_2(List<Person> people){
+        Collections.sort(people,Comparator.comparingInt(Person::getAge));
+    }
+    //多重判断   判断年龄，工作年龄
+    private void sortUsingJava8_3(List<Person> people){
+        Collections.sort(people,Comparator
+                        .comparing(Person::getAge)
+                        .thenComparingInt(Person::getWorkYear));
+    }
+    //TODO
+    private static void sortUsingJava8_4(List<String> names){
+        names = names.stream().sorted(Comparator.comparing(String::toString)).collect(Collectors.toList());
+    }
+
+    /**
+     * int res = o1.first.compareTo(o2.first);
+     * if(res == 0) res = o1.second.compareTo(o2.second);
+     * if(res == 0) res = o1.third - o2.third;
+     * return res;
+     *
+     * will be replaced with
+     * objs.sort(Comparator.comparing((Obj o) -> o.first)
+     * .thenComparing(o -> o.second).thenComparingInt(o -> o.third));
+     */
+
+
+    class Person{
+        private String name;
+        private Integer age;
+        private Integer workYear;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public void setAge(Integer age) {
+            this.age = age;
+        }
+
+        public Integer getWorkYear() {
+            return workYear;
+        }
+
+        public void setWorkYear(Integer workYear) {
+            this.workYear = workYear;
+        }
+    }
+}
