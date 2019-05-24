d90815d826f48a6b550d3e60fae99255ececc061
agfca <a.gjb@outlook.com>
2019-05-24 20:47:02 +0800

    jdk8Lambda

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,6 +1,5 @@
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author guofucheng
@@ -10,6 +9,9 @@ import java.util.List;
 public class MainTest {
 
     public static void main(String[] args) {
+
+
+
         List<Comment> list = new ArrayList<>();
         Comment comment1 = new Comment("111");
         Comment comment2 = new Comment("222");
@@ -21,7 +23,43 @@ public class MainTest {
         list.add(comment3);
         list.add(comment4);
 
+        //通知对象
+        List<Comment> noticeIdList =new ArrayList<Comment>(list);
+
+
+
+        list.removeIf(comment -> comment.getContent().equals("111"));
+
+
+
+        for(int i = 0; i <1000; i++){
+            Comment comment = new Comment(Integer.toString(i));
+            list.add(comment);
+        }
+
+        Integer i = null;
+        System.out.println(i instanceof Integer);
         //list.parallelStream().m
+
+        String[] arr = new String[2];
+        arr[0] = "2";
+        arr[1] = "3";
+        List<String> list1 = new ArrayList<>(Arrays.asList(arr));
+
+        list1.add("22");
+
+        System.out.println(new String[0].length);
+
+        List<Comment> commentList = Collections.synchronizedList(new ArrayList<>());
+
+        AtomicInteger atomicInteger = new AtomicInteger();
+        list.parallelStream().forEach(item ->{
+
+            item.setCreateTime(new Date());
+            //commentList.add(item);
+        });
+
+        System.out.println("---");
     }
 }
 
